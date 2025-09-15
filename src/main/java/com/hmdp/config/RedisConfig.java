a176f0acdcbb48e1857232dd1ba9cfb72385d2f6
vickllny <vickllny@163.com>
2025-09-15 14:53:13 +0800

    p134 begin

--- src/main/java/com/hmdp/config/RedisConfig.java
+++ src/main/java/com/hmdp/config/RedisConfig.java
@@ -1,15 +1,11 @@
 package com.hmdp.config;
 
-import io.lettuce.core.ReadFrom;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @Configuration
 public class RedisConfig {
